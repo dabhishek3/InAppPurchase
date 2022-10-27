d019ebc31d7a7fa8a813f66970f439dbfeed973a
Abhishek Singh <abhisheksinghmimit@gmail.com>
2022-10-27 16:31:07 +0530

    first commit

---
+++ DemoInAppPurchase/DemoInAppPurchase/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Plugin.InAppBilling;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoInAppPurchase
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var productId = "com.appdev.inapppurchase.id";
+            var billing = CrossInAppBilling.Current;
+            try
+            {
+                var connected = await billing.ConnectAsync();
+                if (!connected)
+                {
+                    //we are offline or can't connect, don't try to purchase
+                    //return false;
+                }
+
+                //check purchases
+                var purchase = await billing.PurchaseAsync(productId, ItemType.InAppPurchaseConsumable);
+
+                //possibility that a null came through.
+                if (purchase == null)
+                {
+                    //did not purchase
+                }
+                else if (purchase.State == PurchaseState.Purchased)
+                {
+                    // purchased, we can now consume the item or do it later
+                    // here you may want to call your backend or process something in your app.
+
+                    //only required on Android & Windows    
+                    var wasConsumed = await CrossInAppBilling.Current.ConsumePurchaseAsync(purchase.ProductId, purchase.TransactionIdentifier);
+
+                    if (wasConsumed)
+                    {
+                        //Consumed!!
+                    }
+                }
+            }
+            catch (InAppBillingPurchaseException purchaseEx)
+            {
+                //Billing Exception handle this based on the type
+                Debug.WriteLine("Error: " + purchaseEx);
+            }
+            catch (Exception ex)
+            {
+                //Something else has gone wrong, log it
+                Debug.WriteLine("Issue connecting: " + ex);
+            }
+            finally
+            {
+                await billing.DisconnectAsync();
+            }
+        }
+    }
+}
